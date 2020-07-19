d7d81db4a41ee8d73a49181bae3dbd8d02f267b2
atharvk42 <68445382+atharvk42@users.noreply.github.com>
2020-07-18 23:21:14 -0700

    Update 1.3 rushing_passing_stats.R

--- 1.3 rushing_passing_stats.R
+++ 1.3 rushing_passing_stats.R
@@ -1,28 +1,33 @@
-rushing <- final_data
-passing <- final_data
-
-rushing <-
-  rushing %>%
-  group_by(game_id) %>%
-  filter(play_type == 'run' & winning_team == posteam) %>%
-  
-  mutate(rushing_yards_winning_team = sum(yards_gained)) %>%
-  mutate(ypc = mean(yards_gained)) %>%
-  select(game_id, rushing_yards_winning_team, ypc)
-
-rushing <- rushing[!duplicated(rushing$game_id), ]
-
-passing <- passing %>%
-  group_by(game_id) %>%
-  filter(play_type == 'pass' & winning_team == posteam) %>%
-  mutate(passing_yards_winning_team = sum(yards_gained)) %>%
-  mutate(ypa = mean(yards_gained)) %>%
-  select(game_id, passing_yards_winning_team, ypa)
-
-passing <- passing[!duplicated(passing$game_id), ]
-
 final_data_per_game <- final_data
 final_data_per_game <- final_data_per_game[!duplicated(final_data_per_game$game_id), ]
+
+rushing <- final_data
+rushing <-
+  rushing %>%
+  filter(play_type == 'run' & winning_team == posteam) 
+rushing_yards_per_game <- aggregate(x = rushing$yards_gained,               
+                                    by = list(rushing$game_id),        
+                                    FUN = sum)
+colnames(rushing_yards_per_game) <- c("game_id", "rushing_yards")
+
+rushing <- rushing[!duplicated(rushing$game_id), ]
+rushing <- rushing %>%
+  mutate(rushing_yards_winning_team = rushing_yards_per_game$rushing_yards) %>%
+  select(game_id, rushing_yards_winning_team)
+
+passing <- final_data
+passing <-
+  passing %>%
+  filter(play_type == 'pass' & winning_team == posteam) 
+passing_yards_per_game <- aggregate(x = passing$yards_gained,               
+                                    by = list(passing$game_id),        
+                                    FUN = sum)
+colnames(passing_yards_per_game) <- c("game_id", "passing_yards")
+
+passing <- passing[!duplicated(passing$game_id), ]
+passing <- passing %>%
+  mutate(passing_yards_winning_team = passing_yards_per_game$passing_yards) %>%
+  select(game_id, passing_yards_winning_team)
 
 final_data_per_game <-
   final_data_per_game %>%
@@ -37,14 +42,3 @@ final_data_per_game <-
 final_data_per_game <-
   final_data_per_game %>%
   mutate(percentage_yards_rushing = rushing_yards_winning_team/(rushing_yards_winning_team+passing_yards_winning_team))
-
-ggplot(data = final_data_per_game) + 
-  geom_point(aes(x = rushing_yards_winning_team, y = passing_yards_winning_team, color = winning_team, alpha = 0.2)) +
-  labs(x = "Rushing Yards", y = "Passing Yards") + 
-  geom_abline(intercept = 297.8966416, slope = -0.4396726, color = "black")
-
-cor(final_data_per_game$ypc, final_data_per_game$ypa)
-
-ggplot(data = final_data_per_game) + 
-  geom_point(aes(x = ypc, y = ypa, color = winning_team, alpha = 0.2)) +
-  geom_abline(intercept = 7.32429203, slope = -0.03153615, color = "black")
